f32c0362572b4bdbe5bd386e9c25232bafa1dad1
Code Next <codenext@Codes-MacBook-Air-7.local>
2018-12-08 15:49:17 -0800

    Initial commit

---
+++ app/src/main/java/com/example/codenext/tradingcard/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.codenext.tradingcard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(
+                .layout.activity_main);
+    }
+}
